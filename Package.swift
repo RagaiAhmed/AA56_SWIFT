a7c4aef53388dfeb32a21cae4c3e18b529db4aaa
Ragai Ahmed <ragaiawad@gmail.com>
2022-07-01 20:05:00 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,28 @@
-// swift-tools-version: 5.6
-
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-	name: "CCode"
+    name: "AA56",
+    platforms: [
+        .macOS(.v10_11), .iOS(.v11),
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "AA56",
+            type: .dynamic,
+            targets: ["AA56"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "AA56",
+            dependencies: [],
+            path: "Sources/AA56",
+            exclude: ["*.ini", "*.cat"]
+        )
+    ]
 )
